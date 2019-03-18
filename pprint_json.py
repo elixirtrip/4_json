edcd1611e3d6a1940a8593dd68b1c34251a4f1e8
elixirtrip <elixirtrip@gmail.com>
2019-03-18 21:01:53 +0300

    added parse args

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,43 @@
 import json
+import os
+import argparse
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path, 'r', encoding='utf8') as file:
+        try:
+            return json.load(file)
+        except json.decoder.JSONDecodeError:
+            return None
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4))
+
+
+def get_console_args():
+    parser = argparse.ArgumentParser(
+        prog='Pretty json',
+        description='Script turns compact json data'
+        'into a beautiful and easy to read view'
+    )
+
+    parser.add_argument(
+        '--file_path',
+        default='alco_shops.json',
+        help='You have to specify a file'
+        'path to any file with compact json data'
+    )
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    parse_args = get_console_args()
+    data = load_data(parse_args.file_path)
+    if data is None:
+        sys.exit('File not found or data is not in json format')
+    pretty_print_json(data)
